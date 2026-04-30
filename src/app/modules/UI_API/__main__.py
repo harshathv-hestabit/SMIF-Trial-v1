05c35eff98e3fac5ab6ddcc90f7a6f2a5961d73f
Harshath Venkatesh <harshathv.hestabit@gmail.com>
2026-04-30 15:35:31 +0530

    CHORE: added the Host configuration changes to  UI_API module

--- src/app/modules/UI_API/__main__.py
+++ src/app/modules/UI_API/__main__.py
@@ -6,7 +6,7 @@ from .settings import settings
 if __name__ == "__main__":
     uvicorn.run(
         "app.modules.UI_API.main:app",
-        host="0.0.0.0",
+        host=settings.HOST_URL,
         port=settings.UI_API_PORT,
         log_level="info",
     )
